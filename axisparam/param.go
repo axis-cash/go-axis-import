75edf198cccdf15068d0de296373a1aa0d8073cd
axisxplan <support@axis.cash>
2020-08-24 21:05:29 +0800

    update sip to xip

--- axisparam/param.go
+++ axisparam/param.go
@@ -1,24 +1,22 @@
 package axisparam
 
-func SIP1() uint64 {
+func XIP1() uint64 {
 	if is_dev {
 		return 0
 	} else {
-		//return uint64(130000) //for miner rewards
 		return uint64(0) //for miner rewards
 	}
 }
 
-func SIP2() uint64 {
+func XIP2() uint64 {
 	if is_dev {
 		return 0
 	} else {
-		//return uint64(606006)
 		return uint64(0)
 	}
 }
 
-func SIP3() uint64 {
+func XIP3() uint64 {
 	if is_dev {
 		return uint64(940410)
 	} else {
@@ -30,7 +28,6 @@ func VP1() uint64 {
 	if is_dev {
 		return 0
 	} else {
-		//return uint64(829000)
 		return uint64(0)
 	}
 }
@@ -39,34 +36,30 @@ func VP0() uint64 {
 	if is_dev {
 		return 0
 	} else {
-		//return uint64(788888)
 		return uint64(0)
 	}
 }
 
-func SIP4() uint64 {
+func XIP4() uint64 {
 	if is_dev {
 		return 0
 	} else {
-		//return uint64(1300000)
 		return uint64(0)
 	}
 }
 
-func SIP5() uint64 {
+func XIP5() uint64 {
 	if is_dev {
 		return 0
 	} else {
-		//return uint64(1958696)
 		return uint64(0)
 	}
 }
 
-func SIP6() uint64 {
+func XIP6() uint64 {
 	if is_dev {
 		return 50
 	} else {
-		//return uint64(2123558)
 		return uint64(0)
 	}
 }
@@ -77,7 +70,7 @@ const MAX_O_OUT_LENGTH = int(10)
 
 const MAX_Z_OUT_LENGTH_OLD = int(6)
 
-const MAX_Z_OUT_LENGTH_SIP2 = int(500)
+const MAX_Z_OUT_LENGTH_XIP2 = int(500)
 
 const MAX_CONTRACT_OUT_COUNT_LENGTH = int(256)
 
